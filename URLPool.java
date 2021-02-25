44384b2c1868f76cbddc364148813259b4016721
DonDano-prog <daniililichevbuz@gmail.com>
2021-02-25 18:11:49 +0300

    fixed infinte loop due to incorrect steps to check whether page was visited

--- URLPool.java
+++ URLPool.java
@@ -20,18 +20,18 @@ public final class URLPool
     public synchronized boolean isEmpty() { return queuePairs.size() < 0; }
     public Set<URLPair> getVisitedKeys() { return visitedMap.keySet(); }
     /** Method to add the URL in the queue; it able to add only if this URL was never been visited yet */
-    public synchronized boolean addToQueue(URLPair urlPair) throws Exception
+    public synchronized boolean addToQueue(URLPair urlPair)
     {
-        if(visitedMap.containsKey(urlPair) == true)
-            throw new Exception("already in visited");
         queuePairs.addLast(urlPair);
         /** Notify all threads that thay able to get a task */
         notify();
         return true;
     }
     /** Adding URL to visited; no need to check because we made sure that this URL unvisited earlier */
-    public synchronized void addVisited(URLPair urlPair)
+    public synchronized void addVisited(URLPair urlPair) throws Exception
     {
+        if(visitedMap.containsKey(urlPair) == true)
+            throw new Exception("already in visited");
         visitedMap.put(urlPair, 0);
     }
     /** Getting the first from the queue */
